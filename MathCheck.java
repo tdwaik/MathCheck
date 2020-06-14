4658b05c18d76f302763f740e50b4deec9b0752e
Thaer AlDwaik <tdwaik@users.noreply.github.com>
2020-06-14 02:03:11 -0700

    fix a missing case

--- MathCheck.java
+++ MathCheck.java
@@ -6,13 +6,14 @@ public class MathCheck {
 
     public static void main(String[] args) {
 
-        String bla = "(2*6*8(398/7)-5+(7%5)(5+2*(1+2))+2*(2)+2*2^2((2*2)))";
+        String bla = "2*6*8(398/7)-5+(7%5)(5+2*(1+2))+2*(2)+2*2^2((2*2))";
+        String regex = "\\((\\d+[\\+\\-\\*\\/\\^\\%])+\\d+\\)|\\(\\d+\\)|(\\d+[\\+\\-\\*\\/\\^\\%])+\\d+";
         String replaceWith = "1"; // use only numbers
         String compare;
 
         do {
             compare = bla;
-            bla = bla.replaceAll("\\((\\d+[\\+\\-\\*\\/\\^\\%])+\\d+\\)|\\(\\d+\\)", replaceWith);
+            bla = bla.replaceAll(regex, replaceWith);
         }while (!compare.equals(bla));
 
         System.out.println(bla.equals(replaceWith)? "sa7" : "mush sa7");
